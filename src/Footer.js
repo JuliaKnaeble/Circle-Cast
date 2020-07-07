bff4ec8af0c5d3381ad9a698219b3f9655c499de
Julia.K <knaeble.julia@gmail.com>
2020-07-07 11:17:35 +0200

    Updated Footer with correct repository link

--- src/Footer.js
+++ src/Footer.js
@@ -5,9 +5,9 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer>
-      Made by Julia K. 2020. Code on
+      Made by Julia K. 2020. Code on{" "}
       <a
-        href="https://github.com/JuliaKnaeble/Whats-the-Weather"
+        href="https://github.com/JuliaKnaeble/circle-cast-react"
         target="_blank"
         rel="noopener noreferrer"
       >
