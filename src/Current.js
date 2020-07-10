b0b44f3ed8675b97e69752cddb394ef407a6183c
Julia.K <knaeble.julia@gmail.com>
2020-07-10 12:53:29 +0200

    Setting fixed color (blue) on circles

--- src/Current.js
+++ src/Current.js
@@ -6,13 +6,14 @@ export default function Current(props) {
   let currentTemp = Math.round(props.temperature);
   let currentHumidity = Math.round(props.humidity);
   let currentWind = Math.round(props.wind);
+  let color = { backgroundColor: `#5B73F5` };
 
   return (
     <div className="Current">
       <div className="current-temp">
         <span className="unit">{currentTemp}°C</span>
       </div>
-      <div className="big-circle">
+      <div className="big-circle" style={color}>
         <span className="gradient-overlay">
           <img
             src={require(`./Assets/${props.icon}.png`)}
--- src/ForecastPreview.js
+++ src/ForecastPreview.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./ForecastPreview.css";
 
 export default function ForecastPreview(props) {
+  let color = { backgroundColor: `#5B73F5` };
   let forecastData = {
     day: "Mon",
   };
   return (
     <div className="forecast">
-      <div className="small-circle">
+      <div className="small-circle" style={color}>
         <span className="gradient-overlay-small">
           <img
             src={require(`./Assets/${props.icon}.png`)}
