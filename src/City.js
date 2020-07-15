92dfd87dc69d14aad0c642b7236e4e4d739452e6
Julia.K <knaeble.julia@gmail.com>
2020-07-15 12:47:17 +0200

    Started passing time related props to Time component

--- src/City.js
+++ src/City.js
@@ -7,10 +7,13 @@ export default function City(props) {
   let citySearched = props.city;
   let currentCondition = props.description;
 
+  dayOfWeek = { day };
+  fullHour = { hour };
+
   return (
     <div className="City">
       <div className="city-name">{citySearched}</div>
-      <Time area={props.area} />
+      <Time day={props.dayOfWeek} hour={props.fullHour} />
       <div className="condition">{currentCondition}</div>
     </div>
   );
