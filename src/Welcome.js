8862bbd22c5501e44ba3691cb17bb44577b7e6ad
Julia.K <knaeble.julia@gmail.com>
2020-07-20 11:22:06 +0200

    Added Js and Css components for Welcome page

---
+++ src/Welcome.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div class="wrapper-welcome">
+      <div class="welcome-circle align-welcome"></div>
+      <div class="welcome-text align-welcome">
+        <p>What's the weather like in...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
