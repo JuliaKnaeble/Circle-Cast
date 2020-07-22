b6bffd145fb49ddbfc66f6cc3429b16c03b275d7
Julia.K <knaeble.julia@gmail.com>
2020-07-22 10:38:37 +0200

    Added useHistory to correctly implement component routing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <Header
-        router="/"
         defaultCity="Tel Aviv"
         unitC={{
           color: `#fcfeff`,
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route, useHistory, Link } from "react-router-dom";
 import Loader from "react-loader-spinner";
 
 import axios from "axios";
@@ -12,7 +12,6 @@ import ForecastPreview from "./ForecastPreview";
 import "./Header.css";
 
 const Header = (props) => {
-  const [router, setRouter] = useState(props.router);
   const [city, setCity] = useState(props.defaultCity);
   const [weather, setWeather] = useState({});
   const [day, setDay] = useState(null);
@@ -22,6 +21,7 @@ const Header = (props) => {
   const [fSelected, setFSelected] = useState({});
   const [cSelected, setCSelected] = useState(props.unitC);
   const [ready, setReady] = useState(false);
+  const history = useHistory();
 
   function showTime(response) {
     setDay(response.data.day_of_week);
@@ -31,7 +31,7 @@ const Header = (props) => {
     let offset = parseInt(response.data.utc_offset);
     let totalHour = inte + offset;
     if (totalHour >= 24) {
-      return totalHour - 24;
+      totalHour -= 24;
     } else if (totalHour < 0) {
       totalHour += 24;
     } else if (totalHour === 0) {
@@ -81,12 +81,13 @@ const Header = (props) => {
     let apiKey = `a785b12636ed229463fa77e0a6deb5be`;
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(url).then(showWeather);
-    setCity("");
+    //setCity("");
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    setRouter("/forecast");
+    setReady(false);
+    history.push("/forecast");
     search();
   }
 
@@ -125,182 +126,178 @@ const Header = (props) => {
 
   if (ready) {
     return (
-      <Router>
-        <div>
-          <div className="Header">
-            <div className="page-name">
-              <a href="index.html">
-                <strong>Circle</strong>Cast
-              </a>
-            </div>
-            <div className="search-form">
-              <form onSubmit={handleSubmit}>
-                <button>
-                  <i className="fas fa-search search-icon" />
-                </button>
-                <Link to={router}>
-                  <input
-                    className="text-input"
-                    type="text"
-                    placeholder="Search for a city"
-                    autoComplete="off"
-                    spellCheck="false"
-                    autoFocus
-                    onChange={handleChange}
-                    value={city}
-                  />
-                </Link>
-              </form>
-            </div>
-            <div>
-              <a href="http://">
-                <span
-                  className="conversion-c"
-                  onClick={displayCelcius}
-                  style={cSelected}
-                >
-                  C
-                </span>
-                <span
-                  className="conversion-f"
-                  onClick={displayFahrenheit}
-                  style={fSelected}
-                >
-                  F
-                </span>
-              </a>
-            </div>
+      <div>
+        <div className="Header">
+          <div className="page-name">
+            <Link to="/">
+              <strong>Circle</strong>Cast
+            </Link>
           </div>
-          <Route path="/" exact component={Welcome} />
-          <Route
-            path="/forecast"
-            exact
-            render={(props) => (
-              <City
-                {...props}
-                city={weather.city}
-                description={weather.description}
-                dayOfWeek={day}
-                fullHour={hour.hourSet}
+          <div className="search-form">
+            <form onSubmit={handleSubmit}>
+              <button>
+                <i className="fas fa-search search-icon" />
+              </button>
+              <input
+                className="text-input"
+                type="text"
+                placeholder="Search for a city"
+                autoComplete="off"
+                spellCheck="false"
+                autoFocus
+                onChange={handleChange}
+                value={city}
               />
-            )}
-          />
+            </form>
+          </div>
+          <div>
+            <a href="http://">
+              <span
+                className="conversion-c"
+                onClick={displayCelcius}
+                style={cSelected}
+              >
+                C
+              </span>
+              <span
+                className="conversion-f"
+                onClick={displayFahrenheit}
+                style={fSelected}
+              >
+                F
+              </span>
+            </a>
+          </div>
+        </div>
+        <Route path="/" exact component={Welcome} />
+        <Route
+          path="/forecast"
+          exact
+          render={(props) => (
+            <City
+              {...props}
+              city={weather.city}
+              description={weather.description}
+              dayOfWeek={day}
+              fullHour={hour.hourSet}
+            />
+          )}
+        />
+        <Route
+          path="/forecast"
+          exact
+          render={(props) => (
+            <Current
+              {...props}
+              temperature={weather.temp}
+              humidity={weather.humid}
+              wind={weather.wind}
+              icon={weather.icon}
+              unit={fahrenheit}
+            />
+          )}
+        />
+        <div className="forecast-flax-wrapper forecast-scroll">
           <Route
             path="/forecast"
             exact
             render={(props) => (
-              <Current
+              <ForecastPreview
                 {...props}
-                temperature={weather.temp}
-                humidity={weather.humid}
-                wind={weather.wind}
-                icon={weather.icon}
+                max={forecast.daily[0].temp.max}
+                min={forecast.daily[0].temp.min}
+                icon={forecast.daily[0].weather[0].icon}
+                day={day}
                 unit={fahrenheit}
               />
             )}
           />
-          <div className="forecast-flax-wrapper forecast-scroll">
-            <Route
-              path="/forecast"
-              exact
-              render={(props) => (
-                <ForecastPreview
-                  {...props}
-                  max={forecast.daily[0].temp.max}
-                  min={forecast.daily[0].temp.min}
-                  icon={forecast.daily[0].weather[0].icon}
-                  day={day}
-                  unit={fahrenheit}
-                />
-              )}
-            />
-            <Route
-              path="/forecast"
-              exact
-              render={(props) => (
-                <ForecastPreview
-                  {...props}
-                  max={forecast.daily[1].temp.max}
-                  min={forecast.daily[1].temp.min}
-                  icon={forecast.daily[1].weather[0].icon}
-                  day={day + 1}
-                  unit={fahrenheit}
-                />
-              )}
-            />
-            <Route
-              path="/forecast"
-              exact
-              render={(props) => (
-                <ForecastPreview
-                  {...props}
-                  max={forecast.daily[2].temp.max}
-                  min={forecast.daily[2].temp.min}
-                  icon={forecast.daily[2].weather[0].icon}
-                  day={day + 2}
-                  unit={fahrenheit}
-                />
-              )}
-            />
-            <Route
-              path="/forecast"
-              exact
-              render={(props) => (
-                <ForecastPreview
-                  {...props}
-                  max={forecast.daily[3].temp.max}
-                  min={forecast.daily[3].temp.min}
-                  icon={forecast.daily[3].weather[0].icon}
-                  day={day + 3}
-                  unit={fahrenheit}
-                />
-              )}
-            />
-            <Route
-              path="/forecast"
-              exact
-              render={(props) => (
-                <ForecastPreview
-                  {...props}
-                  max={forecast.daily[4].temp.max}
-                  min={forecast.daily[4].temp.min}
-                  icon={forecast.daily[4].weather[0].icon}
-                  day={day + 4}
-                  unit={fahrenheit}
-                />
-              )}
-            />
-            <Route
-              path="/forecast"
-              exact
-              render={(props) => (
-                <ForecastPreview
-                  {...props}
-                  max={forecast.daily[5].temp.max}
-                  min={forecast.daily[5].temp.min}
-                  icon={forecast.daily[5].weather[0].icon}
-                  day={day + 5}
-                  unit={fahrenheit}
-                />
-              )}
-            />
-            <Route
-              path="/forecast"
-              exact
-              render={(props) => (
-                <ForecastPreview
-                  {...props}
-                  max={forecast.daily[6].temp.max}
-                  min={forecast.daily[6].temp.min}
-                  icon={forecast.daily[6].weather[0].icon}
-                  day={day + 6}
-                  unit={fahrenheit}
-                />
-              )}
-            />
-          </div>
+          <Route
+            path="/forecast"
+            exact
+            render={(props) => (
+              <ForecastPreview
+                {...props}
+                max={forecast.daily[1].temp.max}
+                min={forecast.daily[1].temp.min}
+                icon={forecast.daily[1].weather[0].icon}
+                day={day + 1}
+                unit={fahrenheit}
+              />
+            )}
+          />
+          <Route
+            path="/forecast"
+            exact
+            render={(props) => (
+              <ForecastPreview
+                {...props}
+                max={forecast.daily[2].temp.max}
+                min={forecast.daily[2].temp.min}
+                icon={forecast.daily[2].weather[0].icon}
+                day={day + 2}
+                unit={fahrenheit}
+              />
+            )}
+          />
+          <Route
+            path="/forecast"
+            exact
+            render={(props) => (
+              <ForecastPreview
+                {...props}
+                max={forecast.daily[3].temp.max}
+                min={forecast.daily[3].temp.min}
+                icon={forecast.daily[3].weather[0].icon}
+                day={day + 3}
+                unit={fahrenheit}
+              />
+            )}
+          />
+          <Route
+            path="/forecast"
+            exact
+            render={(props) => (
+              <ForecastPreview
+                {...props}
+                max={forecast.daily[4].temp.max}
+                min={forecast.daily[4].temp.min}
+                icon={forecast.daily[4].weather[0].icon}
+                day={day + 4}
+                unit={fahrenheit}
+              />
+            )}
+          />
+          <Route
+            path="/forecast"
+            exact
+            render={(props) => (
+              <ForecastPreview
+                {...props}
+                max={forecast.daily[5].temp.max}
+                min={forecast.daily[5].temp.min}
+                icon={forecast.daily[5].weather[0].icon}
+                day={day + 5}
+                unit={fahrenheit}
+              />
+            )}
+          />
+          <Route
+            path="/forecast"
+            exact
+            render={(props) => (
+              <ForecastPreview
+                {...props}
+                max={forecast.daily[6].temp.max}
+                min={forecast.daily[6].temp.min}
+                icon={forecast.daily[6].weather[0].icon}
+                day={day + 6}
+                unit={fahrenheit}
+              />
+            )}
+          />
         </div>
-      </Router>
+      </div>
     );
   } else {
     return (
